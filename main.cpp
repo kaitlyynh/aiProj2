d54bd2b3685889d68aa68d1c85df40a060bd6145
Kaitlyn Huynh <87906190+kaitlyynh@users.noreply.github.com>
2024-12-15 14:07:58 -0500

    Update main.cpp
    added comments
    modify dh to use list

--- main.cpp
+++ main.cpp
@@ -1,154 +1,174 @@
 import sys
-# N is the size of the 2D matrix   N*N
-N = 9
 
-    
+# GRID_SIZE is the size of the 2D matrix   GRID_SIZE*GRID_SIZE
+GRID_SIZE = 9
 
-            
-# A utility function to print grid
+
+# A utility function to print a grid
 def printing(arr):
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            print(arr[i][j], end = " ")
+            print(arr[i][j], end=" ")
         print()
     print()
 
-#
-def check_white(grid, row, col, num):
-    other = grid[row][col]
-    if other == 0: return False
+# Verify that white dot constraint is satisfied
+def check_white(other, num):
+    if other == 0: return True
     return abs(other - num) == 1
-    
-def check_black(grid, row, col, num):
-    other = grid[row][col]
-    if other == 0: return False
-    return (other / num) == 0.5 or (other / num) == 2
 
-# verify if white constraint is satisfied horizontally
-def check_white_row(grid, row, col, num):
-    cell1_val = grid[row][col]
-    return abs(cell1_val - num) == 1
-    
-def check_black_row(grid, row, col, num):
-    
-    cell1_val = grid[row][col]
-    return (cell1_val / num) == 0.5 or (cell1_val / num) == 2
+# Verify that black dot constraint is satisfied
+def check_black(other, num):
+    if other == 0: return True
+    return (other * 2 == num) or (num * 2) == other
 
-#verify if constraint is satisfied vertically
-def check_white_col(grid, row, col, num):
-   
-    cell1_val = grid[row][col]
-    return abs(cell1_val - num) == 1
-    
-def check_black_col(grid, row, col, num):
-    cell1_val = grid[row][col]
-    return (cell1_val / num) == 0.5 or (cell1_val / num) == 2
 
-def isSafe(grid, row, col, num, rbw, cbw): # pass in RowBlackWhite (horizontal constraints) matrix
-    
-    for x in range(9):
-        if grid[row][x] == num:
-            return False # violated unique row constraint
+def row_col_subgrid_traverse(grid, row, col, action):
+    """
+    This will traverse the corresponding row, column, and subgrid
+    and call action (a function passed as a parameter) on each cell
+    """
+    for c in range(9):
+        if c == col: continue
+        action(grid[row][c])
 
-    for x in range(9):
-        if grid[x][col] == num:
-            return False # violated unique column constraint
-
+    for r in range(9):
+        if r == row: continue
+        action(grid[r][col])
     startRow = row - row % 3
     startCol = col - col % 3
     for i in range(3):
+        if i == row: continue
         for j in range(3):
-            if grid[i + startRow][j + startCol] == num:
-                return False
+            if j == col: continue
+            action(grid[i + startRow][j + startCol])
 
-   
-    dot_constraints = True
+
+def dot_traverse(grid, row, col, hor_dots, vert_dots, action):
+    """
+    Given a cell, look up all adjacent cells that are
+    involved in dot constraints with it and pass them to
+    action (a function passed as a parameter).
+    """
+    print(f"dot_traverse on r,c = ({row}, {col}), action = {action}")
     # case 1: dot b/w me and left
     # col - 1
-    if 0 <= row < len(rbw) and 0 <= col - 1 < len(rbw[0]):
-        if rbw[row][col - 1] == 1 and not check_white(grid, row, col - 1, num): return False
-        elif rbw[row][col - 1] == 2 and  not  check_black(grid, row, col - 1, num): return False
+    if 0 <= row < len(hor_dots) and 0 <= col - 1 < len(hor_dots[0]):
+        print("dot_traverse case 1")
+        action(hor_dots[row][col - 1], grid[row][col - 1])
+
     # case 2 : dot b/w me and right
-    # col 
-    if 0 <= row < len(rbw) and 0 <= col < len(rbw[0]):
-        if rbw[row][col] == 1 and not check_white(grid, row, col, num): return False
-        elif rbw[row][col] == 2 and  not  check_black(grid, row, col, num): return False
-    #case 3: dot b/w me and above
+    # col
+    if 0 <= row < len(hor_dots) and 0 <= col < len(hor_dots[0]):
+        print("dot_traverse case 2")
+        action(hor_dots[row][col], grid[row][col + 1])
+    # case 3: dot b/w me and above
     # row - 1 
-    if 0 <= row - 1 < len(cbw) and 0 <= col  < len(cbw[0]):
-        if cbw[row - 1][col] == 1 and not check_white(grid, row - 1, col, num): return False
-        elif cbw[row - 1][col] == 2 and not check_black(grid, row - 1, col, num): return False
+    if 0 <= row - 1 < len(vert_dots) and 0 <= col < len(vert_dots[0]):
+        print("dot_traverse case 3")
+        action(vert_dots[row - 1][col], grid[row - 1][col])
     # case 4: dot b/w me and below
     # row
-    if 0 <= row < len(cbw) and 0 <= col < len(cbw[0]):
-        if cbw[row][col] == 1 and not check_white(grid, row, col, num): return False
-        elif cbw[row][col] == 2 and not check_black(grid, row, col, num): return False
-    # print(row, " ,", col, " Dot: ", dot_constraints)
+    if 0 <= row < len(vert_dots) and 0 <= col < len(vert_dots[0]):
+        print("dot_traverse case 4")
+        action(vert_dots[row][col], grid[row + 1][col])
+
+
+def isSafe(grid, row, col, num, hor_dots, vert_dots):
+    """
+    Given the grid, position (row, col), projected cell value indicated
+    by num, and a grid of the vertical and horizontal constraints,
+    determine whether "num" violates any constraints
+    """
+    is_diff = True
+
+    # Check allDiff constraint
+    def diff_check(value):
+        nonlocal is_diff
+        if value == num:
+            is_diff = False
+    # Traverse the cell's row, column, and subgrid to verify allDiff
+    row_col_subgrid_traverse(grid, row, col, diff_check)
+    if not is_diff: return False
+
+    # Check dot constraints
+    def dot_check(dot, cell):
+        nonlocal is_diff
+        if dot == 1 and \
+                not check_white(cell, num):
+            is_diff = False
+        elif dot == 2 and \
+                not check_black(cell, num):
+            is_diff = False
+    # Traverse the cell's adjacent tiles, determine if any dot constraint is violated
+    dot_traverse(grid, row, col, hor_dots, vert_dots, dot_check)
+    if not is_diff: return False
     return True
 
-
-    # if 0 <= row < len(rbw) and 0 <= col - 1 < len(rbw[0]):
-    #     if rbw[row][col - 1] == 1: # check if its white
-    #         return check_white_row(grid, row, col - 1, num)
-    #     elif rbw[row][col - 1] == 2: # check if its black
-    #         return check_black_row(grid, row, col - 1, num)
-
-
-    # if 0 <= row - 1 < len(cbw) and 0 <= col < len(cbw[0]):
-    #     if cbw[row - 1][col] == 1:
-    #         return check_white_col(grid, row - 1, col, num)
-    #     elif cbw[row - 1][col] == 2:
-    #         return check_black_col(grid, row - 1, col, num)
-
-
-
-
-def mrv(grid, rbw, cbw):
+# Construct list of coordinates with smallest domain size
+def mrv(grid, hor_dots, vert_dots):
+    print("IN MRV")
     candidates = []
-    min_legal_states = float("inf")
+    min_legal_states = 10
     # iterate through every cell in the grid and count how many legal states they have
     for r in range(9):
         for c in range(9):
             legal_states = 0
-            if grid[r][c] == 0: # skip assigned cells
-                for val in range(1,10):
-                    if isSafe(grid, r, c, val, rbw, cbw): legal_states += 1
-                #print("legal_states: " , legal_states)
-                if legal_states < min_legal_states:
+            if grid[r][c] == 0:  # skip assigned cells
+                for val in range(1, 10):
+                    if isSafe(grid, r, c, val, hor_dots, vert_dots): legal_states += 1
+                print("legal_states: ", legal_states)
+                if legal_states < min_legal_states: # smaller domain size found
                     candidates = [(r, c)]
                     min_legal_states = legal_states
-                elif legal_states == min_legal_states:
-                    candidates.append((r,c))
+                elif legal_states == min_legal_states: # add to coordinate list
+                    candidates.append((r, c))
     print("mrv candidates: ", candidates)
+    if len(candidates) == 0: print("no mrv's\n")
     return candidates
 
 
-def degree_heuristic(mrv_results, rbw, cbw):
-    candidates = []
+def row_grid_sub_degs(value, deg):
+    print(f"in row_grid_sub_degs value: {value}  deg[0]: {deg[0]}")
+    if value == 0:  # Unassigned cells contribute to the degree
+        deg[0] += 1
+        print("in row_grid_sub_degs incremented", value, deg[0])
+
+def dot_check_degs(dot, cell, deg):
+    print(f"in dot_check_degs dot : {dot} cell : {cell} and deg[0] = {deg[0]}")
+    if dot != 0 and cell == 0:  # Unassigned cells with dot constraints
+        print("in dot_check_degs incremented")
+        deg[0] += 1
+
+
+def degree_heuristic(grid, mrv_results, hor_dots, vert_dots):
+    print("IN DH", mrv_results)
     max_degree = -1
+    max_var = tuple() # track coordinate with most constraints
+
     for coord in mrv_results:
-        degree = 0
-        
+        degree = [0]  # update element within a mutable list
         r, c = coord[0], coord[1]
-        #print(r, c)
-        # horizontal dot constraints              
-        if 0 <= c - 1 < len(rbw[0]) and 0 <= r < len(rbw):
-            if rbw[r][c - 1] != 0: degree += 1 
-        if 0 <= c + 1 < len(rbw[0]) and 0 <= r < len(rbw):
-            if  rbw[r][c] != 0: degree += 1
-        # vertical dot constraints
-        if 0 <= c < len(cbw[0]) and 0 <= r - 1 < len(cbw) :
-            if cbw[r - 1][c] != 0 : degree += 1 
-        if 0 <= c  < len(cbw[0]) and 0 <= r < len(cbw):
-            if cbw[r][c] != 0 : degree += 1
-        #print("degree: " ,degree)
-        if degree > max_degree:
-            candidates = [(r, c)]
-            max_degree = degree
-        elif degree == max_degree:
-            candidates.append((r,c))
-    print("dh candidates: ", candidates)
-    return candidates[0]
+
+        print("before row_col_subgrid_traverse", degree[0], "coord is (", r, c, ")")
+        # pass value of a cell to row_grid_sub_degs using lambda
+        # count degree based on the row, column, and subgrid
+        row_col_subgrid_traverse(grid, r, c, lambda value: row_grid_sub_degs(value, degree))
+        print("degree after row_col_subgrid_traverse", degree[0])
+
+        # pass the dot (hor/vert) and the adjacent cell's value to dot_check_degs using lambda
+        # count degree based on the dot constraints
+        dot_traverse(grid, r, c, hor_dots, vert_dots, lambda dot, cell: dot_check_degs(dot, cell, degree))
+        print("degree after dot_traverse", degree[0])
+
+        # Track the candidate with the highest degree
+        if degree[0] > max_degree:
+            max_degree = degree[0]
+            max_var = coord
+
+    print("coord chosen: ", max_var, "degree is ", degree[0])
+    return max_var
+
 
 def count_zeros(matrix):
     count = 0
@@ -157,92 +177,87 @@ def count_zeros(matrix):
             if matrix[r][c] == 0:
                 count += 1
     return count
-    
-def select_unassigned_variable(grid, rbw, cbw):
-    return degree_heuristic(mrv(grid, rbw, cbw), rbw, cbw)
 
-def solveSudoku(grid, rbw, cbw):
-    
-    # grid is complete
-    #if g_assigned_cells == 81: return True
-    # print(count_zeros(grid))
-    
+# Select a coordinate with the smallest domain and highest number of constraints
+def select_unassigned_variable(grid, hor_dots, vert_dots):
+    return degree_heuristic(grid, mrv(grid, hor_dots, vert_dots), \
+                            hor_dots, vert_dots)
+
+
+def solveSudoku(grid, hor_dots, vert_dots):
+    # Grid is complete
     if count_zeros(grid) == 0: return True
-    # if (row == N - 1 and col == N):
+
+    # if (row == GRID_SIZE - 1 and col == GRID_SIZE):
     #     return True
-    
+
     # Traverse from top to bottom
-    # if col == N:
+    # if col == GRID_SIZE:
     #     row += 1
     #     col = 0
-    
-    row, col = select_unassigned_variable(grid, rbw, cbw)
-    
+
+    # Select the next coordinate to populate
+    row, col = select_unassigned_variable(grid, hor_dots, vert_dots)
     print(row, col)
 
-    # num represents all possible values from [1 to 9] one by one
-    for num in range(1, N + 1):
-
-        if isSafe(grid, row, col, num, rbw, cbw):
-            #print("Here 1")
-            # not doing MRV, not tracking invalid values
-            #coord = degree_heuristic(mrv(grid, rbw, cbw))
-            grid[row][col] = num 
-            if solveSudoku(grid, rbw, cbw):
-                return True 
-        #print("Done", num)
-        grid[row][col] = 0 #undo assignment if backtracking yields no solution
-
-    
-        
+    # num represents all possible values from [1, 9]
+    for num in range(1, GRID_SIZE + 1):
+        # verify whether num violates any constraint
+        if isSafe(grid, row, col, num, hor_dots, vert_dots):
+            grid[row][col] = num # try assignment
+            if solveSudoku(grid, hor_dots, vert_dots):
+                return True
+        grid[row][col] = 0  # undo assignment if backtracking yields no solution
     return False
 
 
 def parse_file():
-    if (len(sys.argv) != 2): 
+    if (len(sys.argv) != 2):
         print("Input file required")
         return []
-    
+
     # Initialize empty matrices
-    matrix_9x9 = [] # grid
-    matrix_9x8 = [] # rbw
-    matrix_8x9 = [] # cbw
-    
+    matrix_9x9 = []  # grid
+    matrix_9x8 = []  # hor_dots
+    matrix_8x9 = []  # vert_dots
+
     with open(sys.argv[1], 'r') as file:
+    # with open("Input2.txt", 'r') as file:
         lines = file.readlines()
-        
+
         # Assume matrices are separated by a blank line
         # Split the content into the three sections based on blank lines
         sections = ''.join(lines).strip().split("\n\n")
-        
+
         # Load 9x9 matrix
         for line in sections[0].splitlines():
             matrix_9x9.append(list(map(int, line.split())))
-        
+
         # Load 9x8 matrix
         for line in sections[1].splitlines():
             matrix_9x8.append(list(map(int, line.split())))
-        
+
         # Load 8x9 matrix
         for line in sections[2].splitlines():
             matrix_8x9.append(list(map(int, line.split())))
-    
+
     return matrix_9x9, matrix_9x8, matrix_8x9
 
 
 def main():
-    grid, rbw, cbw  = parse_file()
-    # printing(grid)
-    # printing(rbw)
-    # printing(cbw)   
+    grid, hor_dots, vert_dots = parse_file()
 
-    
-    if (solveSudoku(grid, rbw, cbw)):
+    # printing(grid)
+    # printing(hor_dots)
+    # printing(vert_dots)   
+
+    if (solveSudoku(grid, hor_dots, vert_dots)):
         printing(grid)
         print()
     else:
         printing(grid)
         print("no solution  exists ")
         print()
-    
+
+
 main()
