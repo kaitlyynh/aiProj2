935b2b3e745a22235af5975c15cd8518f262c2cf
Kaitlyn Huynh <87906190+kaitlyynh@users.noreply.github.com>
2024-12-16 14:05:55 -0500

    Update main.cpp
    Final Version

--- main.cpp
+++ main.cpp
@@ -50,27 +50,22 @@ def dot_traverse(grid, row, col, hor_dots, vert_dots, action):
     involved in dot constraints with it and pass them to
     action (a function passed as a parameter).
     """
-    print(f"dot_traverse on r,c = ({row}, {col}), action = {action}")
     # case 1: dot b/w me and left
     # col - 1
     if 0 <= row < len(hor_dots) and 0 <= col - 1 < len(hor_dots[0]):
-        print("dot_traverse case 1")
         action(hor_dots[row][col - 1], grid[row][col - 1])
 
     # case 2 : dot b/w me and right
     # col
     if 0 <= row < len(hor_dots) and 0 <= col < len(hor_dots[0]):
-        print("dot_traverse case 2")
         action(hor_dots[row][col], grid[row][col + 1])
     # case 3: dot b/w me and above
     # row - 1 
     if 0 <= row - 1 < len(vert_dots) and 0 <= col < len(vert_dots[0]):
-        print("dot_traverse case 3")
         action(vert_dots[row - 1][col], grid[row - 1][col])
     # case 4: dot b/w me and below
     # row
     if 0 <= row < len(vert_dots) and 0 <= col < len(vert_dots[0]):
-        print("dot_traverse case 4")
         action(vert_dots[row][col], grid[row + 1][col])
 
 
@@ -107,7 +102,6 @@ def isSafe(grid, row, col, num, hor_dots, vert_dots):
 
 # Construct list of coordinates with smallest domain size
 def mrv(grid, hor_dots, vert_dots):
-    print("IN MRV")
     candidates = []
     min_legal_states = 10
     # iterate through every cell in the grid and count how many legal states they have
@@ -117,56 +111,43 @@ def mrv(grid, hor_dots, vert_dots):
             if grid[r][c] == 0:  # skip assigned cells
                 for val in range(1, 10):
                     if isSafe(grid, r, c, val, hor_dots, vert_dots): legal_states += 1
-                print("legal_states: ", legal_states)
                 if legal_states < min_legal_states: # smaller domain size found
                     candidates = [(r, c)]
                     min_legal_states = legal_states
                 elif legal_states == min_legal_states: # add to coordinate list
                     candidates.append((r, c))
-    print("mrv candidates: ", candidates)
-    if len(candidates) == 0: print("no mrv's\n")
     return candidates
 
 
 def row_grid_sub_degs(value, deg):
-    print(f"in row_grid_sub_degs value: {value}  deg[0]: {deg[0]}")
     if value == 0:  # Unassigned cells contribute to the degree
         deg[0] += 1
-        print("in row_grid_sub_degs incremented", value, deg[0])
 
 def dot_check_degs(dot, cell, deg):
-    print(f"in dot_check_degs dot : {dot} cell : {cell} and deg[0] = {deg[0]}")
     if dot != 0 and cell == 0:  # Unassigned cells with dot constraints
-        print("in dot_check_degs incremented")
         deg[0] += 1
 
 
 def degree_heuristic(grid, mrv_results, hor_dots, vert_dots):
-    print("IN DH", mrv_results)
     max_degree = -1
     max_var = tuple() # track coordinate with most constraints
 
     for coord in mrv_results:
         degree = [0]  # update element within a mutable list
         r, c = coord[0], coord[1]
-
-        print("before row_col_subgrid_traverse", degree[0], "coord is (", r, c, ")")
         # pass value of a cell to row_grid_sub_degs using lambda
         # count degree based on the row, column, and subgrid
         row_col_subgrid_traverse(grid, r, c, lambda value: row_grid_sub_degs(value, degree))
-        print("degree after row_col_subgrid_traverse", degree[0])
 
         # pass the dot (hor/vert) and the adjacent cell's value to dot_check_degs using lambda
         # count degree based on the dot constraints
         dot_traverse(grid, r, c, hor_dots, vert_dots, lambda dot, cell: dot_check_degs(dot, cell, degree))
-        print("degree after dot_traverse", degree[0])
 
         # Track the candidate with the highest degree
         if degree[0] > max_degree:
             max_degree = degree[0]
             max_var = coord
 
-    print("coord chosen: ", max_var, "degree is ", degree[0])
     return max_var
 
 
@@ -184,28 +165,19 @@ def select_unassigned_variable(grid, hor_dots, vert_dots):
                             hor_dots, vert_dots)
 
 
-def solveSudoku(grid, hor_dots, vert_dots):
+def backtrack(grid, hor_dots, vert_dots):
     # Grid is complete
     if count_zeros(grid) == 0: return True
 
-    # if (row == GRID_SIZE - 1 and col == GRID_SIZE):
-    #     return True
-
-    # Traverse from top to bottom
-    # if col == GRID_SIZE:
-    #     row += 1
-    #     col = 0
-
     # Select the next coordinate to populate
     row, col = select_unassigned_variable(grid, hor_dots, vert_dots)
-    print(row, col)
 
     # num represents all possible values from [1, 9]
     for num in range(1, GRID_SIZE + 1):
         # verify whether num violates any constraint
         if isSafe(grid, row, col, num, hor_dots, vert_dots):
             grid[row][col] = num # try assignment
-            if solveSudoku(grid, hor_dots, vert_dots):
+            if backtrack(grid, hor_dots, vert_dots):
                 return True
         grid[row][col] = 0  # undo assignment if backtracking yields no solution
     return False
@@ -222,7 +194,6 @@ def parse_file():
     matrix_8x9 = []  # vert_dots
 
     with open(sys.argv[1], 'r') as file:
-    # with open("Input2.txt", 'r') as file:
         lines = file.readlines()
 
         # Assume matrices are separated by a blank line
@@ -247,17 +218,12 @@ def parse_file():
 def main():
     grid, hor_dots, vert_dots = parse_file()
 
-    # printing(grid)
-    # printing(hor_dots)
-    # printing(vert_dots)   
-
-    if (solveSudoku(grid, hor_dots, vert_dots)):
+    if (backtrack(grid, hor_dots, vert_dots)):
         printing(grid)
         print()
     else:
         printing(grid)
         print("no solution  exists ")
         print()
-
 
 main()
